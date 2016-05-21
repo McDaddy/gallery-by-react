4a0caccf2337471c3b6585a1f1c4b6c78986db0f
McDaddy <mcdaddychen@126.com>
2016-05-21 21:16:47 +0800

    edit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.less');
 
 let imageDatas =  require('../data/imageDatas.json');
 
@@ -19,13 +19,36 @@ let getImageURL = (imageDataArr) =>{
 imageDatas = getImageURL(imageDatas);
 console.log(111, imageDatas);
 
+let ImgFigure = React.createClass({
+  render: function(){
+    return (
+      <figure className='img-figure'>
+        <img src={this.props.data.imageUrl}/>
+        <figcaption>
+        <h2 className='img-title'>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+      );
+  }
+});
+
 class AppComponent extends React.Component {
   render() {
+    let controllerUnits = [];
+    let pics = [];
+
+    imageDatas.forEach( function(value) {
+      pics.push(<ImgFigure data={value} />);
+    });
+    console.log(111,pics);
+
     return (
       <section className="stage">
         <section className='img-sec'>
+          {pics}
         </section>
         <nav className='controller-nav'>
+          {controllerUnits}
         </nav>
       </section>
     );
